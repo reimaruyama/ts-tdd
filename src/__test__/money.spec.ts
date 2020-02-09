9766307ba31e489ba8ee40bc9e0335690d96f102
Rei Maruyama <rei.maruyama@serverworks.co.jp>
2020-02-09 18:25:21 +0900

    5

--- src/__test__/money.spec.ts
+++ src/__test__/money.spec.ts
@@ -1,4 +1,5 @@
-import Dollar from '../dollar'
+import Dollar from '../dollar';
+import Franc from '../franc';
 
 describe('Dollar', () => {
   describe("#times", () => {
@@ -16,6 +17,26 @@ describe('Dollar', () => {
     })
     test('$5 != $6', () => {
       expect(new Dollar(5)).not.toEqual(new Dollar(6));
+    })
+  })
+})
+
+describe('Franc', () => {
+  describe("#times", () => {
+    test('$5 * 2 = $10, $5 * 3 = $15', () => {
+      const five = new Franc(5);
+
+      expect(five.times(2)).toEqual(new Franc(10));
+
+      expect(five.times(3)).toEqual(new Franc(15));
+    })
+  })
+  describe('#equals', () => {
+    test('$5 == $5', () => {
+      expect(new Franc(5)).toEqual(new Franc(5));
+    })
+    test('$5 != $6', () => {
+      expect(new Franc(5)).not.toEqual(new Franc(6));
     })
   })
 })
